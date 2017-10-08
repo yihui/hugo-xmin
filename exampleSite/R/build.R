ffa1147b46cfdf04c9674eb420183bdd78f07a55
Yihui Xie <xie@yihui.name>
2017-10-08 09:23:40 -0500

    bash and sed may not be available on Windows

--- exampleSite/R/build.R
+++ exampleSite/R/build.R
@@ -1,5 +1,5 @@
 file.copy('content/_index.markdown', '../README.md', overwrite = TRUE)
-system("sed -i '' -e '1,6d' -e '34,38d' ../README.md")
+if (Sys.which('sed') != '') system("sed -i '' -e '1,6d' -e '34,38d' ../README.md")
 cat(
   '[![Screenshot](https://github.com/yihui/hugo-xmin/raw/master/images/screenshot.png)](https://xmin.yihui.name)\n',
   file = '../README.md', append = TRUE
